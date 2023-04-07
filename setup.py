36a26059f1b4561151d15c6db393a5074689e99f
ChristianSquadro <christian.squadro@gmail.com>
2023-04-07 18:54:33 +0200

    First commit

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+from glob import glob
+
+
+package_name = 'thymio_sim'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        
+                ## NOTE: you must add this line to use launch files
+        # Instruct colcon to copy launch files during package build 
+        ('share/' + package_name + '/launch', glob('launch/*.launch.*')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='robotics23',
+    maintainer_email='robotics23@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        'controller_thymio_node = thymio_sim.controller_thymio_node:main'
+        ],
+    },
+)
