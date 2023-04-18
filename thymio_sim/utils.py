a020418f039a611f0f3e1b2aa42d78d30c732040
nicholas_carlotti <carlon@usi.ch>
2023-04-18 16:21:11 +0200

    Added some transformation utils.

--- thymio_sim/utils.py
+++ thymio_sim/utils.py
@@ -1,4 +1,19 @@
 from collections import Counter
+import numpy as np
+
+def mkrot(theta):
+    return np.array([
+        [np.cos(theta), -np.sin(theta), 0],
+        [np.sin(theta), np.cos(theta), 0],
+        [0, 0, 1]
+        ])
+
+def mktransl(xt, yt):
+    return np.array([
+        [1, 0, xt],
+        [0, 1, yt],
+        [0, 0, 1]
+        ])
 
 def list_mode(lst):
     data = Counter(lst)
